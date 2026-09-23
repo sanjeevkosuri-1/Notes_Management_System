1cb45f4833f4f48c1ad682666ea096315c6ed219
KOSURI SANJEEVA NAIDU <sanjeevkosuri@gmail.com>
2026-09-23 21:22:11 +0530

    Refactor MongoDB connection handling
    Removed server listening log and export app.

--- server/server.js
+++ server/server.js
@@ -27,12 +27,9 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
     })
     .catch((error) => {
         console.log("MongoDB connection failed");
         console.log(error.message);
     });
+module.exports=app;
